2b62a91157e14252fe88a80e0e39844b859ee65d
ZeioNara <zeionara@gmail.com>
2017-11-23 13:03:20 +0300

    Added application crud for workers_statuses

--- python_crud/applications.py
+++ python_crud/applications.py
@@ -2,8 +2,11 @@ import cx_Oracle
 import configparser
 import datetime
 
-commands = (('get_schedule','master id, date of visit in format 10.10.2010'),
-            ('get_schedule','master id'))
+commands = (('get_schedule','gs', 'master_id [date_of_visit_in_format_10.10.2010]'),
+            ('create_worker_state','cws','state_name [state_description]'),
+            ('delete_worker_state','dws','state_id'),
+            ('update_worker_state','uws','state_id [-n new_name] [new_description]'))
+
 
 def establish_connection(ip, port, sid):
     config = configparser.ConfigParser()
@@ -12,12 +15,24 @@ def establish_connection(ip, port, sid):
     dsn_tns = cx_Oracle.makedsn(ip, port, sid)
 
     db = cx_Oracle.connect(config['helios']['login'], config['helios']['password'], dsn_tns, encoding = "UTF-8")
-    return db.cursor()
+    return (db.cursor(), db)
+
+def notify(operation, ident):
+    print("%s item with id %i" % (operation, ident))
+
+def notify_insert(ident):
+    notify("Inserted",ident)
+
+def notify_delete(ident):
+    notify("Deleted",ident)
+
+def notify_update(ident):
+    notify("Updated",ident)
 
 def list_all_commands():
-    print('%-20s %-50s' % ('name','arguments'))
+    print('%-20s %-10s %-50s' % ('name','short name','arguments'))
     for command in commands:
-        print('%-20s %-50s' % command)
+        print('%-20s %-10s %-50s' % command)
 
 def handle_get_schedule(command, cursor):
     if len(command) == 3:
@@ -30,19 +45,59 @@ def handle_get_schedule(command, cursor):
     for row in cursor:
         print("%-10s %-20s %-20s %-20s" % row);
 
+def handle_create_worker_state(command, cursor, connection):
+    if len(command) == 2:
+        res = cursor.callfunc('WORKERS_STATES_tapi.ins_f', cx_Oracle.NUMBER, (None, command[1]))
+        conn.commit()
+    elif len(command) > 2:
+        res = cursor.callfunc('WORKERS_STATES_tapi.ins_f', cx_Oracle.NUMBER, (" ".join(command[2:]), command[1]))
+        conn.commit()
+    notify_insert(res)
+    #print("Inserted item with id %i" % res)
+
+def handle_delete_worker_state(command, cursor, connection):
+    if len(command) >= 2:
+        res = cursor.callproc('WORKERS_STATES_tapi.del', [command[1]])
+        conn.commit()
+    notify_delete(res)
+    #print("Deleted item with id %s" % res[0])
+
+def handle_update_worker_state(command, cursor, connection):
+    if (len(command) == 4) and (command[2] == '-n'):
+        res = cursor.callproc('WORKERS_STATES_tapi.upd', (None, int(command[1]), command[3]))
+        conn.commit()
+        res = int(command[1])
+    elif (len(command) > 4) and (command[2] == '-n'):
+        res = cursor.callproc('WORKERS_STATES_tapi.upd', (" ".join(command[4:]), int(command[1]), command[3]))
+        conn.commit()
+        res = int(command[1])
+    elif (len(command) > 4):
+        res = cursor.callproc('WORKERS_STATES_tapi.upd', (" ".join(command[2:]), int(command[1]), None))
+        conn.commit()
+        res = int(command[1])
+    notify_update(res)
+    #print("Updated item with id %i" % res) 
 
 #main
 
 if __name__ == '__main__':
-
-    cursor = establish_connection('127.0.0.1',1521,'orbis')
-
+    result = establish_connection('127.0.0.1',1521,'orbis')
+    cursor = result[0]
+    
+    conn = result[1]
     while(True):
         command = input('\nType a command (\'list\' to get available commands or \'exit\' to exit): \n\n').split(' ');
         print('\n\nresult:\n\n');
         if command[0] == 'list':
             list_all_commands()
-        elif command[0] == 'get_schedule':
+        elif command[0] == 'gs':
             handle_get_schedule(command, cursor)
+        elif command[0] == 'cws':
+            handle_create_worker_state(command, cursor, conn)
+        elif command[0] == 'dws':
+            handle_delete_worker_state(command, cursor, conn)
+        elif command[0] == 'uws':
+            handle_update_worker_state(command, cursor, conn)
         elif command[0] == 'exit':
             break;
+    
