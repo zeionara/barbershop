2e3f492fbdc29ed79d695edf3daff0f500f9eaf3
ZeioNara <zeionara@gmail.com>
2017-12-04 21:41:56 +0300

    Started making api

--- python_mongo_crud/crud/connection.py
+++ python_mongo_crud/crud/connection.py
@@ -1,5 +1,10 @@
 from ming import create_datastore
 from ming.odm import ThreadLocalODMSession
 
+session = None
+
 def create_session(login, password, path):
-    return ThreadLocalODMSession( bind = create_datastore('mongodb://%s:%s@%s' % (login, password, path)))
+    global session
+    if session == None:
+        session = ThreadLocalODMSession( bind = create_datastore('mongodb://%s:%s@%s' % (login, password, path)))
+    return session
--- python_mongo_crud/crud/holdings.py
+++ python_mongo_crud/crud/holdings.py
@@ -4,6 +4,7 @@ from connection import create_session
 from ming import schema
 from ming.odm import FieldProperty
 from ming.odm.declarative import MappedClass
+from ming.odm.mapper import MapperExtension
 
 from commons import EnhancingClass
 
@@ -14,17 +15,32 @@ config.read('C://Users//Zerbs//accounts.sec')
 
 session = create_session(config['mongo']['login'], config['mongo']['password'], config['mongo']['path'])
 
+class HoldingTriggers(MapperExtension):
+    
+    def validate(self, obj):
+        if (obj.quantity <= 0):
+            raise ValueError("Quantity must be more than zero")
+        if (obj.price <= 0):
+            raise ValueError("Price must be more than zero")
+    
+    def before_insert(self, obj, st, sess):
+        self.validate(obj)
+        
+    def before_update(self, obj, st, sess):
+        self.validate(obj)
+
 class Holding(MappedClass, EnhancingClass):
     
     class __mongometa__:
         session = session
         name = collection_name
+        extensions = [HoldingTriggers]
     
     _id = FieldProperty(schema.ObjectId)
     name = FieldProperty(schema.String(required=True))
     quantity = FieldProperty(schema.Float(required=True))
     price = FieldProperty(schema.Float(required=True))
     
-holding = Holding(name = "small scissors", quantity = 50, price = 200.5)
+#holding = Holding(name = "small scissors", quantity = 50, price = 200.5)
 
-session.flush_all()
+#session.flush_all()
