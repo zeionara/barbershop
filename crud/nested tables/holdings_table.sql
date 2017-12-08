4aafcd7acc90dad15bb32da8a6ea843ee45b9254
Nastya <ostanina.anastasia2014@yandex.ru>
2017-12-08 12:16:19 +0300

    fix

--- crud/nested tables/holdings_table.sql
+++ crud/nested tables/holdings_table.sql
@@ -8,16 +8,15 @@ declare
 end; 
 --(https://asktom.oracle.com/pls/apex/asktom.search?tag=nested-table-20010812) 
 
-
 create or replace package nest_holdings_tapi
 is
 
 -- insert
-procedure ins (request_id REQUESTS.ID%type,  holdings_id HOLDINGS.ID%type, holdings_quantity HOLDINGS.quantity%type);
+procedure ins (request_id REQUESTS.ID%type,  holdings_id HOLDINGS.ID%type, holdings_quantity HOLDINGS.QUANTITY%type);
 --delete
 procedure del (request_id REQUESTS.ID%type,  holdings_id HOLDINGS.ID%type);
 --update
-procedure upd(request_id REQUESTS.ID%type,  old_holdings_id HOLDINGS.ID%type, new_holdings_id HOLDINGS.ID%type);
+procedure upd(request_id REQUESTS.ID%type, new_holdings_id HOLDINGS.ID%type, new_holdings_quantity HOLDINGS.QUANTITY%type);
 
 end nest_holdings_tapi;
 
@@ -25,25 +24,25 @@ end nest_holdings_tapi;
 create or replace package body nest_holdings_tapi
 is
 -- insert
-procedure ins (request_id REQUESTS.ID%type, holdings_id HOLDINGS.ID%type) is
+procedure ins (request_id REQUESTS.ID%type, holdings_id HOLDINGS.ID%type, holdings_quantity HOLDINGS.QUANTITY%type) is
 begin
-  insert into table(select holdings from requests where id = 1) values(holding__(2,8));
+  insert into table(select holdings from requests where id = request_id) values(holding__(holdings_id,holdings_quantity));
 end;
 
 --delete
 procedure del (request_id REQUESTS.ID%type, holdings_id HOLDINGS.ID%type) is
 begin
-  delete from table(select holdings from requests where id = 1) where id = 2;
+  delete from table(select holdings from requests where id = request_id) where id = holdings_id;
 end;
 
 --update
-procedure upd(request_id REQUESTS.ID%type,  old_holdings_id HOLDINGS.ID%type, new_holdings_id HOLDINGS.ID%type) is 
+procedure upd(request_id REQUESTS.ID%type, new_holdings_id HOLDINGS.ID%type, new_holdings_quantity HOLDINGS.QUANTITY%type) is 
 begin
 	update requests 
-	set holdings = holdings_table__(holding__(1,2)) 
-	where id = 1; 
+	set holdings = holdings_table__(holding__(new_holdings_id,new_holdings_quantity)) 
+	where id = request_id; 
 end;
- 
+
 end nest_holdings_tapi;
 /
 
